cb0fec55d1faf26b86a2bed589abfcdb6aa19e4c
Daniel Revere <dcprevere@gmail.com>
2016-02-29 18:54:59 +0000

    add headings and editor links

--- link-maker.sh
+++ link-maker.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/bash
+
+# shells
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
+
+# wm
 ln -s ~/.dotfiles/.i3rc ~/.i3/config
+
+# editors
+ls -s ~/.dotfiles/.vimrc ~/.vimrc
+ls -s ~/.dotfiles/.emacs ~/.emacs
